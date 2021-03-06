f0924a3fc4f929fef95b98db46620cabfc30be3d
abhis018 <76279317+abhis018@users.noreply.github.com>
2021-03-07 01:13:14 +0530

    edit button styles
    Signed-off-by: abhis018 <76279317+abhis018@users.noreply.github.com>

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -7,19 +7,23 @@ const Body = ({userData, inputPage}) => {
     return (
         <div className="Get-data">
             {userData.length !== 0 
-                ? <div className='box'>{userData.map((user, i) => (
-                    <Card
-                        key={i}
-                        id={userData[i].id}
-                        FirstName={userData[i].first_name}
-                        LastName={userData[i].last_name}
-                        Email={userData[i].email}
-                    />))}
-                </div>
-                :(inputPage === '' || inputPage === '1' || inputPage === '2'
-                    ? <h2 className="data-info">PRESS SUBMIT BUTTON</h2> 
-                    : <h2 className="data-info">OOPS! SORRY THIS PAGE DOESN'T EXIST</h2>
-                )
+                ?   <div className='box'>{userData.map((user, i) => (
+                        <Card
+                            key={i}
+                            id={userData[i].id}
+                            FirstName={userData[i].first_name}
+                            LastName={userData[i].last_name}
+                            Email={userData[i].email}
+                        />))}
+                    </div>
+                :   <div>
+                        <h2>Enter valid page number and press Submit</h2>
+                        <h5>Note: If data is not showing then either you have not enter a valid page number or you have not pressed the submit button</h5>
+                    </div>
+                // :(inputPage === '' || inputPage === '1' || inputPage === '2'
+                //     ? <h2 className="data-info">PRESS SUBMIT BUTTON</h2> 
+                //     : <h2 className="data-info">OOPS! SORRY THIS PAGE DOESN'T EXIST</h2>
+                // )
             }
         </div>
     )
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = ({Submit, onInputChange}) => {
     return (
         <div className="header">
             <h1>TECHIE&nbsp;&nbsp;&nbsp;&nbsp;BIO-DATA</h1>
-            <input placeholder="enter page number" onChange={onInputChange}/>
+            <input placeholder="Enter page number" onChange={onInputChange}/>
             <button onClick={Submit}>Submit</button>
         </div>
     )
